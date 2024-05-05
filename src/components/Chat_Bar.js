c9c7d82449022666b545cafe1439ef08ed01394e
wmccrthy <wyattmccarthy2020@gmail.com>
2024-05-05 13:51:42 -0400

    conversational use of gemini + ui fix

--- src/components/Chat_Bar.js
+++ src/components/Chat_Bar.js
@@ -1,18 +1,29 @@
 import React, {useState, useRef, useEffect} from "react";
 import ChatItem from "./ChatItem";
-
+import {CiTrash} from 'react-icons/ci'
 
 
 const ChatBar = () => {
     const [chatItems, setChatItems] = useState([])
     const chatBottom = useRef();
 
-    const API_key = "enter your own"
+    const API_key = "AIzaSyDbYafelS05UpJY63Q9PT5-tEmxJzwotcA"
     const { GoogleGenerativeAI } = require("@google/generative-ai");
 
     // Access your API key as an environment variable (see "Set up your API key" above)
     const genAI = new GoogleGenerativeAI(API_key);
     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+    const [chat, setChat] = useState(model.startChat({
+        history: []
+    }))
+
+    const handleReset = () => {
+        setChat(model.startChat({
+            history: []
+        }))
+        setChatItems([])
+    }
     
     useEffect(() => {
         chatBottom.current?.scrollIntoView({behavior:'smooth'})
@@ -20,25 +31,33 @@ const ChatBar = () => {
 
     const handleChat = async (text) => {
     // add user chat item to chatWindow
-    chatItems.push([text, "User"])
-    var newItems = chatItems.slice()
-    setChatItems(newItems)
+        chatItems.push([text, "User"])
+        var newItems = chatItems.slice()
+        setChatItems(newItems)
 
-    // query Gemini for response and add chat item to chatWindow 
-    // console.log(chatItems)
-    try {
-        const gemini_result = await model.generateContent(text)
-        const response = await gemini_result.response;
-        const answer = response.text()
-        console.log(response)
-        console.log(answer)
-        chatItems.push([answer, "Botimus Prime"])
-        newItems = chatItems.slice()
-    } catch (err) {
-        chatItems.push(["Sorry, can't help with that (API error)", "Botimus Prime"])
-        newItems = chatItems.slice()
-    }
-    setChatItems(newItems)
+        // query Gemini for response and add chat item to chatWindow 
+        // console.log(chatItems)
+        try {
+            const conv_result = await chat.sendMessage(text)
+            const conv_response = await conv_result.response;
+            const conv_answer = conv_response.text();
+            console.log(conv_answer)
+            const chat_hist = await chat.getHistory();
+            console.log(chat_hist)
+            chatItems.push([conv_answer, "Botimus Prime"])
+            
+            // const gemini_result = await model.generateContent(text)
+            // const response = await gemini_result.response;
+            // const answer = response.text()
+            // // console.log(response)
+            // console.log(answer)
+            // chatItems.push([answer, "Botimus Prime"])
+            newItems = chatItems.slice()
+        } catch (err) {
+            chatItems.push(["Sorry, can't help with that (API error)", "Botimus Prime"])
+            newItems = chatItems.slice()
+        }
+        setChatItems(newItems)
     }
 
     const handleSubmit = (e) => {
@@ -60,7 +79,7 @@ const ChatBar = () => {
                 ))}
                 <div ref={chatBottom}></div>
             </div>
-
+            
             <form className="input w-5/6 h-8 rounded-lg flex" onSubmit={handleSubmit}>
                 {/* ROUNDED SEARCH BAR WHERE YOU TYPE QUERY */}
                 <input className="w-full h-full rounded-lg rounded-r-none p-2 border-2 border-sky border-r-0 outline-none" type="text" />
@@ -68,6 +87,10 @@ const ChatBar = () => {
                     Chat
                 </button>
             </form>
+
+            <button className="bg-cyan-800 w-fit h-fit border-2 rounded-lg border-sky text-sky-50 absolute top-2 right-2 text-lg p-1" onClick={handleReset}>
+                    <CiTrash></CiTrash>
+            </button>
         </div>
         </>
     )
